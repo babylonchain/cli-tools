9ee02ad7ae624ff277f0254e39db0e3e0bb52f4e
KonradStaniec <konrad.staniec@gmail.com>
2024-05-08 10:03:19 +0200

    cleanup

--- cmd/createStakingTxCmd.go
+++ cmd/createStakingTxCmd.go
@@ -16,19 +16,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// --magic-bytes 62627434 \
-// --staker-pk $staker_pk \
-// --staking-amount 1000000 \
-// --staking-time 21 \
-// --covenant-committee-pks ffeaec52a9b407b355ef6967a7ffc15fd6c3fe07de2844d61550475e7a5233e5 \
-// --covenant-committee-pks a5c60c2188e833d39d0fa798ab3f69aa12ed3dd2f3bad659effa252782de3c31 \
-// --covenant-committee-pks 59d3532148a597a2d05c0395bf5f7176044b1cd312f37701a9b4d0aad70bc5a4 \
-// --covenant-committee-pks 57349e985e742d5131e1e2b227b5170f6350ac2e2feb72254fcc25b3cee21a18 \
-// --covenant-committee-pks c8ccb03c379e452f10c81232b41a1ca8b63d0baf8387e57d302c987e5abb8527 \
-// --covenant-quorum 3 \
-// --network regtest \
-// --finality-provider-pk 03d5a0bb72d71993e435d6c5a70e2aa4db500a62cfaae33c56050deefee64ec0" | jq .staking_tx_hex)
-
 var (
 	FlagMagicBytes           = "magic-bytes"
 	FlagStakerPk             = "staker-pk"
@@ -312,16 +299,3 @@ var createStakingTxCmd = &cobra.Command{
 		return nil
 	},
 }
-
-// --magic-bytes 62627434 \
-// --staker-pk $staker_pk \
-// --staking-amount 1000000 \
-// --staking-time 21 \
-// --covenant-committee-pks ffeaec52a9b407b355ef6967a7ffc15fd6c3fe07de2844d61550475e7a5233e5 \
-// --covenant-committee-pks a5c60c2188e833d39d0fa798ab3f69aa12ed3dd2f3bad659effa252782de3c31 \
-// --covenant-committee-pks 59d3532148a597a2d05c0395bf5f7176044b1cd312f37701a9b4d0aad70bc5a4 \
-// --covenant-committee-pks 57349e985e742d5131e1e2b227b5170f6350ac2e2feb72254fcc25b3cee21a18 \
-// --covenant-committee-pks c8ccb03c379e452f10c81232b41a1ca8b63d0baf8387e57d302c987e5abb8527 \
-// --covenant-quorum 3 \
-// --network regtest \
-// --finality-provider-pk 03d5a0bb72d71993e435d6c5a70e2aa4db500a62cfaae33c56050deefee64ec0" | jq .staking_tx_hex)
